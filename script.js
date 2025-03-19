e8aaf6a8f186cd7aed88b23853a799724af9fec6
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-19 14:50:20 -0400

    finish effect

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const menuPage = document.getElementById("menupage");
 const hyperspace = document.getElementById("hyperspace");
+const trolley = document.getElementById("trolley");
 
 const beginButton = document.getElementById("begin-button");
 
 beginButton.onclick = function () {
   $(menuPage).fadeOut(1000);
   $(hyperspace).fadeIn(1000);
+
+  setTimeout(function () {
+    $(hyperspace).fadeOut(1000);
+    $(trolley).fadeIn(1000);
+  }, 1000);
 }
