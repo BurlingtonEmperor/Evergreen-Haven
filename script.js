9600c54bfa5ee61a5afc0d6e646b4046046bf213
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-20 11:23:13 -0400

    make secure

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const menuPage = document.getElementById("menupage");
 const hyperspace = document.getElementById("hyperspace");
+const drivein = document.getElementById("drivein");
 const trolley = document.getElementById("trolley");
 
 const beginButton = document.getElementById("begin-button");
 
+let interACTIVE = 0;
 beginButton.onclick = function () {
+  switch (interACTIVE) {
+    case 0:
+      interACTIVE = 1;
+      break;
+    case 1:
+      return 0;
+  }
+
   $(menuPage).fadeOut(1000);
   $(hyperspace).fadeIn(1000);
 
   setTimeout(function () {
     $(hyperspace).fadeOut(1000);
     $(trolley).fadeIn(1000);
+    $(drivein).fadeIn(1000);
+
+    setTimeout(function () {
+      interACTIVE = 0;
+    }, 1000);
   }, 1000);
 }
