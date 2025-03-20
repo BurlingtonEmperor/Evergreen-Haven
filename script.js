0c3684e29d67cb64d3cddfd658e368320182bade
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-20 19:32:02 -0400

    make effect

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const plants = document.getElementById("plants");
 const beginButton = document.getElementById("begin-button");
 const gardenButton = document.getElementById("garden-button");
 
+const trolleyButton = document.getElementById("trolley-button"); // for garden
+
 let interACTIVE = 0;
 beginButton.onclick = function () {
   switch (interACTIVE) {
@@ -49,6 +51,30 @@ gardenButton.onclick = function () {
     $(hyperspace).fadeOut(1000);
     $(plants).fadeIn(1000);
     $(garden).fadeIn(1000);
+
+    setTimeout(function () {
+      interACTIVE = 0;
+    }, 1000);
+  }, 1000);
+}
+
+trolleyButton.onclick = function () {
+  switch (interACTIVE) {
+    case 0:
+      interACTIVE = 1;
+      break;
+    case 1:
+      return 0;
+  }
+
+  $(garden).fadeOut(1000);
+  $(plants).fadeOut(1000);
+  $(hyperspace).fadeIn(1000);
+
+  setTimeout(function () {
+    $(hyperspace).fadeOut(1000);
+    $(trolley).fadeIn(1000);
+    $(drivein).fadeIn(1000);
 
     setTimeout(function () {
       interACTIVE = 0;
