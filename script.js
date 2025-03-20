d288f5e0e4b879da0c06bede408ced91f37d4149
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-20 19:22:06 -0400

    fade in garden

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const menuPage = document.getElementById("menupage");
 const hyperspace = document.getElementById("hyperspace");
 const drivein = document.getElementById("drivein");
 const trolley = document.getElementById("trolley");
+const garden = document.getElementById("garden");
+const plants = document.getElementById("plants");
 
 const beginButton = document.getElementById("begin-button");
 const gardenButton = document.getElementById("garden-button");
@@ -45,5 +47,11 @@ gardenButton.onclick = function () {
 
   setTimeout(function () {
     $(hyperspace).fadeOut(1000);
+    $(plants).fadeIn(1000);
+    $(garden).fadeIn(1000);
+
+    setTimeout(function () {
+      interACTIVE = 0;
+    }, 1000);
   }, 1000);
 }
