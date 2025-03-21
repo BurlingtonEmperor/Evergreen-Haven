8fe61641cda586d3382707456815d942de287a2b
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-21 18:11:39 -0400

    add plant selector

--- garden.js
+++ garden.js
@@ -1,15 +1,15 @@
-const fruitingRow = document.getElementById("row1-farm");
-const greensRow = document.getElementById("row2-farm");
-const carrotsRow = document.getElementById("row3-farm");
+// const fruitingRow = document.getElementById("row1-farm");
+// const greensRow = document.getElementById("row2-farm");
+// const carrotsRow = document.getElementById("row3-farm");
 
-for (let i = 0; i < 14; i++) {
-  fruitingRow.innerHTML += "<img src='./roxstrait/fruit.png' width='7%'/>";
-}
+// for (let i = 0; i < 14; i++) {
+//   fruitingRow.innerHTML += "<img src='./roxstrait/fruit.png' width='7%'/>";
+// }
 
-for (let i = 0; i < 14; i++) {
-  greensRow.innerHTML += "<img src='./roxstrait/spinach.png' width='7%' />";
-}
+// for (let i = 0; i < 14; i++) {
+//   greensRow.innerHTML += "<img src='./roxstrait/spinach.png' width='7%' />";
+// }
 
-for (let i = 0; i < 14; i++) {
-  carrotsRow.innerHTML += "<img src='./roxstrait/carrot.png' width='7%' />";
-}
+// for (let i = 0; i < 14; i++) {
+//   carrotsRow.innerHTML += "<img src='./roxstrait/carrot.png' width='7%' />";
+// }
