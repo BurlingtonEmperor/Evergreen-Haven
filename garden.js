88c1aa7670c34509e7ac4b7383ae68b6d3fe22e9
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-21 11:47:07 -0400

    fin 2

--- garden.js
+++ garden.js
@@ -2,6 +2,6 @@ const fruitingRow = document.getElementById("row1-farm");
 const greensRow = document.getElementById("row2-farm");
 const carrotsRow = document.getElementById("row3-farm");
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 14; i++) {
   fruitingRow.innerHTML += "<img src='./roxstrait/fruit.png' width='7%'/>";
 }
