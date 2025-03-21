148a34b288605ae5f389840c8ad0d2fd7053ab2c
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-21 11:44:42 -0400

    resize

--- garden.js
+++ garden.js
@@ -2,7 +2,7 @@ const fruitingRow = document.getElementById("row1-farm");
 const greensRow = document.getElementById("row2-farm");
 const carrotsRow = document.getElementById("row3-farm");
 
-let rowPlantWidth = document.body.width / 5;
+let rowPlantWidth = document.body.width / 15;
 for (let i = 0; i < 5; i++) {
   fruitingRow.innerHTML += "<img src='./roxstrait/fruit.png' width='" + rowPlantWidth + "px'/>";
 }
