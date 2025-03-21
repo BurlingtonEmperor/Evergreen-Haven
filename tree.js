dbb61641c46c46e8be4fa54b721a142caeda4476
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-21 14:53:56 -0400

    18

--- tree.js
+++ tree.js
@@ -3,7 +3,7 @@ const meetinghouse = document.getElementById("meetinghouse");
 
 function percentHeight (elem) {
   let pa = elem.offsetParent || elem;
-  return (((elem.offsetHeight/pa.offsetHeight)*100).toFixed(2) - 14 )+'%';
+  return (((elem.offsetHeight/pa.offsetHeight)*100).toFixed(2) - 18 )+'%';
 }
 
 tree.style.bottom = percentHeight(meetinghouse);
