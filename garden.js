a3d9bc2c8829589a744870d547f7ce74b7b57a1d
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-21 11:44:02 -0400

    beg garden script

---
+++ garden.js
@@ -0,0 +1,8 @@
+const fruitingRow = document.getElementById("row1-farm");
+const greensRow = document.getElementById("row2-farm");
+const carrotsRow = document.getElementById("row3-farm");
+
+let rowPlantWidth = document.body.width / 5;
+for (let i = 0; i < 5; i++) {
+  fruitingRow.innerHTML += "<img src='./roxstrait/fruit.png' width='" + rowPlantWidth + "px'/>";
+}
