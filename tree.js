e024c5e782efe107ce4fee8605188a3ff43e7219
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-21 14:46:04 -0400

    treescript

---
+++ tree.js
@@ -0,0 +1,9 @@
+const tree = document.getElementById("tree");
+const meetinghouse = document.getElementById("meetinghouse");
+
+function percentHeight (elem) {
+  let pa = elem.offsetParent || elem;
+  return 10 + ((elem.offsetHeight/pa.offsetHeight)*100).toFixed(2)+'%';
+}
+
+tree.style.bottom = percentHeight(meetinghouse);
