88701faf0749b040306aa6998c10c25e1b75b9b4
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-21 21:44:51 -0400

    bluh blu

--- tree.js
+++ tree.js
@@ -1,9 +1,9 @@
-const tree = document.getElementById("tree");
-const meetinghouse = document.getElementById("meetinghouse");
+// const tree = document.getElementById("tree");
+// const meetinghouse = document.getElementById("meetinghouse");
 
-function percentHeight (elem) {
-  let pa = elem.offsetParent || elem;
-  return (((elem.offsetHeight/pa.offsetHeight)*100).toFixed(2) - 18 )+'%';
-}
+// function percentHeight (elem) {
+//   let pa = elem.offsetParent || elem;
+//   return (((elem.offsetHeight/pa.offsetHeight)*100).toFixed(2) - 18 )+'%';
+// }
 
-tree.style.bottom = percentHeight(meetinghouse);
+// tree.style.bottom = percentHeight(meetinghouse);
