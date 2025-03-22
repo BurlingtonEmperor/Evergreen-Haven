8a44e678bb75f08e6dc2a93d80039836828e428b
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-21 22:44:44 -0400

    nett

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ const suburbia = document.getElementById("suburbia");
 const neighborhood = document.getElementById("neighborhood");
 const concord = document.getElementById("concord");
 const townhall = document.getElementById("townhall");
+const market = document.getElementById("market");
+const shopping = document.getElementById("shopping");
 
 const beginButton = document.getElementById("begin-button");
 const gardenButton = document.getElementById("garden-button");
 const suburbButton = document.getElementById("suburb-button");
 const hallButton = document.getElementById("hall-button");
+const shoppingButton = document.getElementById("shopping-button");
 
 const trolleyButton = document.getElementById("trolley-button"); // for garden
 const trolleyButtonN = document.getElementById("trolley-buttonn"); // for neighborhood
 const trolleyButtonO = document.getElementById("trolley-buttone"); // town hall
+const trolleyButtonD = document.getElementById("trolley-buttond"); // shopping
 
 let interACTIVE = 0;
 beginButton.onclick = function () {
@@ -83,6 +87,30 @@ hallButton.onclick = function () {
     $(hyperspace).fadeOut(1000);
     $(concord).fadeIn(1000);
     $(townhall).fadeIn(1000);
+
+    setTimeout(function () {
+      interACTIVE = 0;
+    }, 1000);
+  }, 1000);
+}
+
+shoppingButton.onclick = function () {
+  switch (interACTIVE) {
+    case 0:
+      interACTIVE = 1;
+      break;
+    case 1:
+      return 0;
+  }
+
+  $(trolley).fadeOut(1000);
+  $(drivein).fadeOut(1000);
+  $(hyperspace).fadeIn(1000);
+
+  setTimeout(function () {
+    $(hyperspace).fadeOut(1000);
+    $(market).fadeIn(1000);
+    $(shopping).fadeIn(1000);
 
     setTimeout(function () {
       interACTIVE = 0;
@@ -179,6 +207,30 @@ trolleyButtonO.onclick = function () {
 
   $(concord).fadeOut(1000);
   $(townhall).fadeOut(1000);
+  $(hyperspace).fadeIn(1000);
+
+  setTimeout(function () {
+    $(hyperspace).fadeOut(1000);
+    $(trolley).fadeIn(1000);
+    $(drivein).fadeIn(1000);
+
+    setTimeout(function () {
+      interACTIVE = 0;
+    }, 1000);
+  }, 1000);
+}
+
+trolleyButtonD.onclick = function () {
+  switch (interACTIVE) {
+    case 0:
+      interACTIVE = 1;
+      break;
+    case 1:
+      return 0;
+  }
+
+  $(market).fadeOut(1000);
+  $(shopping).fadeOut(1000);
   $(hyperspace).fadeIn(1000);
 
   setTimeout(function () {
