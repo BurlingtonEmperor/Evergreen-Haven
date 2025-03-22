9c661094c0bce6b343c77f2410c71fc2a5aa8474
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-21 21:31:58 -0400

    make red

--- garden.js
+++ garden.js
@@ -32,9 +32,9 @@ function plantCycle () {
   timeTillFert -= 1;
   timeTillHarvest -= 1;
 
-  waterLevel.innerText = "Time until next watering is " + timeTillWater + " minutes";
-  fertLevel.innerText = "Time until next fertilization is " + timeTillFert + " minutes";
-  harvestLevel.innerText = "Time until you can harvest is " + timeTillHarvest + " minutes";
+  waterLevel.innerText = "Time until next watering is <a style='color:red'>" + timeTillWater + "</a> minutes";
+  fertLevel.innerText = "Time until next fertilization is <a style='color:red'>" + timeTillFert + "</a> minutes";
+  harvestLevel.innerText = "Time until you can harvest is <a style='color:red'>" + timeTillHarvest + "</a> minutes";
 }
 
 simStart.onclick = function () {
